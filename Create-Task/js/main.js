c2c05e0dffb751998e03ee05168661739a7038ab
GlassPainter <ckwong6599@gmail.com>
2023-02-13 14:02:20 -0500

    2/13/2023

--- Create-Task/js/main.js
+++ Create-Task/js/main.js
@@ -7,26 +7,59 @@
 // If the color name of the div selected is correct, end game
 // If not, delete original div, then keep going until correct
 
-import { colorArray } from "./array";
-import { newArray } from "./array";
+const colorArray = {
+  Blue: "#0000FF",
+  Red: "#FF0000",
+  Green: "#00FF00",
+  Yellow: "#FFFF00",
+  Orange: "#FFA500",
+  Black: "#000000",
+  Gray: "#808080",
+  Azure: "#0080FF",
+  Brown: "#964B00",
+  Beige: "#F5F5DC",
+  Violet: "#7F00FF",
+  Indigo: "#4B0082",
+  Almond: "#EADDCA",
+  Charcoal: "#36454F",
+  Coffee: "#6F4E37",
+  Coral: "#FF7F50",
+  Denim: "#6F8FAF",
+};
+
+let newArray = [];
 
 let entries = Object.entries(colorArray);
+
 function test() {
   for (let i = 0; i < 4; i++) {
     let random = entries[Math.floor(Math.random() * entries.length)];
-    let newId = `testing${[i]}`;
-    while (newArray.includes(random[0])) {
-      i = i - 1;
+    let newId = `option${i}`;
+    if (newArray.includes(random[0])) {
+      i -= 1;
+      console.log("No good");
+    } else {
+      document
+        .getElementById("testing")
+        .insertAdjacentHTML(
+          "beforeend",
+          `<div class="type" id="${newId}"></div>`
+        );
+      document.getElementById(newId).style.backgroundColor = `${random[1]}`;
+      newArray.push(`${random[0]}`);
     }
-    document
-      .getElementById("testing")
-      .insertAdjacentHTML(
-        "beforeend",
-        `<div class="type" id="${newId}"></div>`
-      );
-    document.getElementById(newId).style.backgroundColor = `${random[1]}`;
-    newArray.push(`${random[0]}`);
+    document.getElementById("name").textContent = random[0];
   }
 }
 
 test();
+
+document.getElementById("option0").addEventListener("click", function () {
+  console.log("Hi");
+});
+
+let startTime = performance.now();
+
+let endTime = performance.now();
+
+console.log(`Call to doSomething took ${endTime - startTime} milliseconds`);
